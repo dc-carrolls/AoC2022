42bd739b84d7116190fa0bfec96d9a9da0069f6c
Steven Carroll <32237935+dc-carrolls@users.noreply.github.com>
2022-12-13 10:32:25 +0000

    Day 3 - Final

--- day3/backpack2.py
+++ day3/backpack2.py
@@ -7,22 +7,18 @@ def priority(c):
   else:
     return -1
 
-file1 = open('.\day3\input.txt', 'r')
-Lines = file1.readlines()
-total = 0
-counter = 0
-for bp in Lines:
-  if counter % 3 == 0: 
-    bp1 = bp.strip()
-    counter += 1
-  elif counter % 3 == 1: 
-    bp2 = bp.strip()
-    counter += 1
-  else:
-    counter = 0
-    bp3 = bp.strip()
-    m = next(c for c in bp1 if c in bp2 and c in bp3 )
+def main():
+  file1 = open('.\day3\input.txt', 'r')
+  Lines = file1.readlines()
+  total = 0
+  l = 0
+  while l < len(Lines):
+    m = next(c for c in Lines[l] if c in Lines[l+1] and c in Lines[l+2] )
     total = total + priority(m)
-print(total)
+    l += 3
+  print(total)
+
+if __name__ == "__main__":
+  main()
 
 
